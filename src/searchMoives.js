490e839f9fe74f6cdb121bd865d43103e1cf5b2b
Danial-Waheed <daniyalwahied@gmail.com>
2022-03-08 23:00:51 +0500

    displayed movie info

--- src/searchMoives.js
+++ src/searchMoives.js
@@ -38,6 +38,30 @@ export default function SearchMoives() {
           Search
         </button>
       </form>
+
+      <div className="card-list">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <div className="card" key={movie.id}>
+              <img
+                className="card--image"
+                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                alt="movie poster"
+              />
+              <div className="card-content">
+                <h3 className="card--title">{movie.title}</h3>
+                <p>
+                  <small>RELEASE DATA: {movie.release_date}</small>
+                </p>
+                <p>
+                  <small>RATING: {movie.vote_average}</small>
+                </p>
+                <p className="card--desc">{movie.overview}</p>
+              </div>
+            </div>
+          ))}
+      </div>
     </>
   );
 }
