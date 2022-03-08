87885fc005167a2b9b6e48552f3f831bdc96a8de
Danial-Waheed <daniyalwahied@gmail.com>
2022-03-08 22:42:06 +0500

    created the search movie function

--- src/searchMoives.js
+++ src/searchMoives.js
@@ -1,9 +1,24 @@
 import React from "react";
 
 export default function searchMoives() {
+  const searchMoives = async (e) => {
+    e.preventDefault();
+    console.log("mitting");
+    const query = "Joker";
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=a662712626815555702f1c6320550397&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
-      <form className="form">
+      <form className="form" onSubmit={searchMoives}>
         <label className="label" htmlFor="query">
           Movie Name
         </label>
